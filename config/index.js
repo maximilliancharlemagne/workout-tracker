21a00f522b959b8935f7fd05a5f56dfbdcab57f8
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-06-12 15:05:02 -0400

    updated config for heroku deployment

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-module.exports = require('mongoose').connect('mongodb://localhost/workout', {
+module.exports = require('mongoose').connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
