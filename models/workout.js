3119a02e08123b5d4e49fefc5cb92616560389f0
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-06-12 10:02:26 -0400

    get route works more?

--- models/workout.js
+++ models/workout.js
@@ -1,14 +1,5 @@
 const { Schema, model } = require('mongoose')
 
 module.exports = model('exercise', new Schema({
-  type: String,
-  workouts: [{
-  type: String,
-  name: String,
-  distance: Number,
-  duration: Number,
-  weight: Number,
-  sets: Number,
-  reps: Number
-}]
+  day: Number
 }))
